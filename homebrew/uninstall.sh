0172d1b5b2822d942a4efae3d1830410f984912a
Pablo Ordorica Wiener <pablo.ordorica.wiener@retailbusinessservices.com>
2022-12-09 20:12:30 -0500

    Script to uninstall Homebrew and packages

---
+++ homebrew/uninstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "⬇️  Uninstalling Homebrew..."
+read -r -q "REPLY?Are you sure you want to proceed? (y/n) ";
+echo "";
+
+if test "$(brew --version)"
+  then
+  sudo -v
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)" \
+  && \
+  echo "🔁 Deleting /opt/homebrew" && \
+  sudo rm -rf /opt/homebrew && \
+  echo "⭐️ Uninstalled Homebrew!"
+else
+  echo "⭐️ Homebrew is already uninstalled!"
+fi
