419ab3e08aa374f5bc190761d74e507b73efd3ec
Pablo Ordorica Wiener <pablo.ordorica.wiener@retailbusinessservices.com>
2022-12-09 21:09:53 -0500

    uninstall cask apps

--- homebrew/uninstall.sh
+++ homebrew/uninstall.sh
@@ -15,3 +15,18 @@ if test "$(brew --version)"
 else
   echo "⭐️ Homebrew is already uninstalled!"
 fi
+
+echo "⬇️ Uinstalling applications with Homebrew..."
+read -r -q "REPLY?Are you sure you want to proceed? (y/n) ";
+echo "";
+brew uninstall --cask dozer
+brew uninstall --cask google-chrome
+brew uninstall --cask iterm2
+brew uninstall --cask lastpass
+brew uninstall --cask miniconda
+brew uninstall --cask notion
+brew uninstall --cask rocket
+brew uninstall --cask slack
+brew uninstall --cask spectacle
+brew uninstall --cask spotify
+brew uninstall --cask visual-studio-code
