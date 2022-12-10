b447de3c419dbde034bf3104b01820450629b3a7
Pablo Ordorica Wiener <pablo.ordorica.wiener@retailbusinessservices.com>
2022-12-09 21:37:59 -0500

    fix reset dock to defaults

--- macos/default-dock.sh
+++ macos/default-dock.sh
@@ -1,7 +1,8 @@
 function reset_dock {
   # reset macOS Dock to default settings
-
-  defaults write com.apple.dock; killall Dock
+  
+  # https://www.defaults-write.com/reset-mac-dock-default/
+  defaults delete com.apple.dock; killall Dock
 }
 
 reset_dock
