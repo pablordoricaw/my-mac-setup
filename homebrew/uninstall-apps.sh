6991c125ee2e1573fe00d86f066951fd83fb9c26
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2024-06-04 18:23:45 -0600

    feat: uninstall apps

--- homebrew/uninstall-apps.sh
+++ homebrew/uninstall-apps.sh
@@ -5,17 +5,23 @@ read -r -q "REPLY?Are you sure you want to proceed? (y/n) ";
 echo "";
 
 if test "$(brew --version)" ; then
+  brew uninstall --cask nikitabobko/tap/aerospace
+  brew uninstall --cask adobe-acrobat-pro
   brew uninstall --cask dozer
+  brew uninstall --cask elgato-control-center
   brew uninstall --cask google-chrome
   brew uninstall --cask iterm2
   brew uninstall --cask lastpass
   brew uninstall --cask miniconda
   brew uninstall --cask notion
+  brew uninstall --cask rectangle
   brew uninstall --cask rocket
+  brew uninstall --cask shapr3d
   brew uninstall --cask slack
-  brew uninstall --cask spectacle
+  brew uninstall --cask snapmaker-luban
   brew uninstall --cask spotify
   brew uninstall --cask visual-studio-code
+  brew uninstall --cask zoom
   echo "⭐️ Uninstalled Applications installed with Homebrew!"
 else
   echo "❌ Can't uninstall applications"
